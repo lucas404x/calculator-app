980bb1d513711398a924b4a0fe5f2417daee1f83
José Lucas <joselucas0303@gmail.com>
2020-04-17 23:56:54 -0300

    all colors from app

---
+++ lib/consts/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsButtons {
+
+  static const NUMBERS_COLOR = Color.fromRGBO(235, 235, 245, 100);
+  static const EQUAL_OPERATOR_COLOR = Color.fromRGBO(36, 210, 54, 10);
+  static const OPERATORS_COLOR = Color.fromRGBO(232, 123, 56, 10);
+  static const CLEAR_COLOR = Color.fromRGBO(255, 235, 235, 100);
+
+}
